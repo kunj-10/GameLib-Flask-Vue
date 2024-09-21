345ccbe3d0bf53a355cac12a76a837e0e5b92f62
kunj_10 <kunjvgoyal30@gmail.com>
2024-09-21 18:24:39 +0530

    Created the templates folder and set up base.html

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+from flask import render_template
 from dotenv import load_dotenv
 
 load_dotenv('./.flaskenv')
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
