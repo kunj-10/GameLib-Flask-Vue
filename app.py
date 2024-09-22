6f22b69d1fc2111976bbb899a70b6fd8e253c7c4
kunj_10 <kunjvgoyal30@gmail.com>
2024-09-23 02:18:48 +0530

    Setting up Database Setting up sqlite database using ORM model SQLAlchemy

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+
 from dotenv import load_dotenv
+
+from config import Config
 
 load_dotenv('./.flaskenv')
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
